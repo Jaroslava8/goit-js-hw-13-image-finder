9bf8e1c4800e107774138787f9fd4bfee1daff59
Jaroslava8 <jaroslava.moroziuk@gmail.com>
2021-11-04 18:30:21 +0000

    key added

---
+++ src/apiService.js
@@ -0,0 +1,34 @@
+const KEY = '24105351-e5a7df18b28acbe554309b74d';
+
+export default class ApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    this.perPage = 12;
+  }
+
+  fetchPictures(e) {
+    
+    const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=${this.perPage}&key=${KEY}`
+
+
+    return fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        this.page += 1;
+        return data.hits;
+      });
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,73 @@
-import './sass/main.scss';
+import ApiService from './apiService.js';
+import cardTmpl from './template.hbs';
+// import * as _ from "lodash";
+import './styles.css';
+import * as PNotify from '@pnotify/core';
+import * as PNotifyMobile from '@pnotify/mobile';
+import '@pnotify/core/dist/BrightTheme.css';
+const inputNotice = new PNotify.Stack({
+  dir1: 'up',
+});
+
+const container = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('[data-action="load-more"]');
+
+const apiService = new ApiService();
+
+window.addEventListener('keyup', onInput);
+loadMoreBtn.addEventListener('click', onLoadMore);
+
+function onInput(e) {
+  if (e.which > 12) {
+    e.preventDefault();
+    apiService.query = e.target.value;
+    if (apiService.query === '') {
+      return;
+    }
+    apiService.resetPage();
+    apiService
+      .fetchPictures()
+      .then(data => {
+        clearContainer();
+        container.insertAdjacentHTML('beforeend', cardTmpl(data));
+        loadMoreBtn.disabled = false;
+        if (data.length === 0) {
+          PNotify.notice({
+            text: 'Unfortunately no matches have been found. Please try again',
+            stack: inputNotice,
+            modules: new Map([...PNotify.defaultModules, [PNotifyMobile, {}]]),
+          });
+        }
+      })
+      .catch(onTheError);
+  }
+}
+function onTheError(typeInfo, text) {
+  container.innerHTML = '';
+  PNotify.notice({
+    text: 'No matches found!',
+    delay: 2000,
+    color: 'red',
+  });
+}
+
+function onLoadMore() {
+  apiService.fetchPictures().then(data => {
+    container.insertAdjacentHTML('beforeend', cardTmpl(data));
+    scroll();
+  });
+}
+
+function clearContainer() {
+  container.innerHTML = '';
+  inputNotice.close(true);
+}
+
+function scroll() {
+  let scrollPhotos = document.querySelectorAll('.galleryList');
+
+  scrollPhotos[scrollPhotos.length - (apiService.perPage - 1)].scrollIntoView({
+    behavior: 'smooth',
+    block: 'end',
+  });
+}
